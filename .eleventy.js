a86f2c98aab0c686ad44ed842b87e2ffdf1b7a9d
Iain Wilson <iain.am.wilson@gmail.com>
2024-04-22 10:40:12 -0500

    Design clean up; added 11ty Navigation

--- .eleventy.js
+++ .eleventy.js
@@ -3,36 +3,53 @@ const navigationPlugin = require('@11ty/eleventy-navigation')
 const rssPlugin = require('@11ty/eleventy-plugin-rss')
 
 module.exports = function(eleventyConfig) {
+
   function filterTagList(tags) {
+
     return (tags || []).filter(tag => ["all", "nav"].indexOf(tag) === -1);
+
   }
+
   eleventyConfig.setDataDeepMerge(true);
 
   function filterTagList(tags) {
+
     return (tags || []).filter(tag => ["all", "nav", "post", "posts"].indexOf(tag) === -1);
+
   }
 
   eleventyConfig.addFilter("filterTagList", filterTagList)
 
   eleventyConfig.addCollection("tagList", collection => {
+
     const tagsObject = {}
     collection.getAll().forEach(item => {
+
       if (!item.data.tags) return;
       item.data.tags
         .filter(tag => !['post', 'all'].includes(tag))
         .forEach(tag => {
+
           if(typeof tagsObject[tag] === 'undefined') {
+
             tagsObject[tag] = 1
+
           } else {
+
             tagsObject[tag] += 1
+
           }
+
         });
     });
 
     const tagList = []
     Object.keys(tagsObject).forEach(tag => {
+
       tagList.push({ tagName: tag, tagCount: tagsObject[tag] })
+
     })
+
     return tagList.sort((a, b) => b.tagCount - a.tagCount)
 
   });
@@ -56,6 +73,9 @@ module.exports = function(eleventyConfig) {
       zone: 'utc'
     }).toFormat('yyyy-LL-dd');
   });
+
+  eleventyConfig.addPlugin(navigationPlugin);
+
   return {
     dir: {
       input: "src",
